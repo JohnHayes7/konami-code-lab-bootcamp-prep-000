708ee5c9df46eeca1c24c1ec63989d6cc2322487
John Hayes <johnnyhayes7@gmail.com>
2018-10-04 23:05:12 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,17 +12,20 @@ const codes = [
 ];
 
 function init() {
-  let idx = 0
-
-  document.body.addEventListener("keydown", (e) => {
-    const key = e.key
+  let index = 0;
+  
+  document.body.addEventListener('keydown', function(e){
+    const key = e.key;
     
-    idx = (codes[idx] === key) ? ++idx : 0
-    
-    if (idx === codes.length) {
-      window.alert("Hurray!");
-      idx = 0
+    if (key === codes[index]){
+      index++;
+      
+      if (index === codes.length){
+        window.alert('Congrats you entered the Konami code');
+        index = 0;
+      }
+    }else{
+      index = 0;
     }
-    
   });
 }
